b14a97d72b6d23a0fbd42229d43e5654ca44a8ab
wilsonli5000 <wilsonli5000@gmail.com>
2016-03-09 13:19:42 -0800

    Change python_tips to a .py file

--- python_tips.py
+++ python_tips.py
@@ -3,3 +3,8 @@ eg. image_index[0:] = [image[0].split() for image in image_index[0:]]
 
 Pandas data frame: each row has an index, access row need iloc[] func if we pass an integer, or use loc[''] is we pass a label. 
 
+Determine whether an element is in list:
+
+3 in [1, 2, 3] :: return true
+
+
